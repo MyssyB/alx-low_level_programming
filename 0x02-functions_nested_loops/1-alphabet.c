bd1c5583f4d26550045f704958393186a9204912
myssyb <beulahagiande@gmail.com>
2023-06-21 13:39:10 -0700

    LEarning the alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* print_alphabet - prints the alphabet in lowercase.
-* Return: Always 0 (Success)
-*/
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Sucess)
+ */
 void print_alphabet(void)
 {
-int i;
-for (i = 97; i <=122; i++)
-{_putchar(i);
-}
-_putchar("\n");
+	int i;
+	for (i = 97; i <= 122; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
 }
