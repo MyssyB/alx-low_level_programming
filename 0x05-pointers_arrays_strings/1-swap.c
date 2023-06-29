a65787239eee6a258a36cc2f921f2516bcac4d7b
myssyb <beulahagiande@gmail.com>
2023-06-29 15:06:20 -0700

    I'm finally fixing all the mistakes!

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * swap_int  - swaps the value of 2 integers a and b.
- * *a - is this first integer to be swapped with the integer b.
- * *b - is the second integer to be swapped with the interger a.
+ * @a: first integer to be swapped with the integer b.
+ * @b: second integer to be swapped with the interger a.
  *
  * Return: Always 0.
  */
