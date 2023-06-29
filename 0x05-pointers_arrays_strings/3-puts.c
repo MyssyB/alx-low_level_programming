daa6cae63104926c25c5b60be62f4ae4597cf5b1
myssyb <beulahagiande@gmail.com>
2023-06-29 14:57:53 -0700

    puts ahs finnaly put!

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - prints a string with a new line to std output.
- * @str - string to be printed on the new line.
- * Return: Always 0.
+ *
+ * @str: Is the string to be printed on the new line.
+ *
+ * Return: Nothing on Success
  */
 void _puts(char *str)
 {
