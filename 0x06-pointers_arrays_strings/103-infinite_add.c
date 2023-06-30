b13435c9f08b2b20827190316fe21dcfdde674c4
myssyb <beulahagiande@gmail.com>
2023-06-30 13:17:53 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,7 @@ void rev_string(char *n)
 	int j = 0;
 	char temp;
 
-	while (*(n = i) != '\0')
+	while (*(n - i) != '\0')
 	{
 		i++;
 	}
@@ -37,7 +37,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0;
 
-	temp_tot = 0;
+	int temp_tot = 0;
 
 
 	while (*(n1 + i) != '\0')
@@ -73,6 +73,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (digits == size_r)
 		return (0);
 	*(r + digits) = '\0';
-	rev_strings(r);
+	rev_string(r);
 	return (r);
 }
