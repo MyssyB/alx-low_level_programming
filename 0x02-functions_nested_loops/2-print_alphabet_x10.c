c896e8a2d49d61a5256fa00ae145c2e96208bfe8
myssyb <beulahagiande@gmail.com>
2023-07-04 13:55:12 -0700

    the aplhabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,21 @@
  * Return: Always 0 (Sucess)
  */
 void print_alphabet_x10(void)
-{	int i;
-	int j;
-	for (j = 1; i <= 10; i++)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
 	{
-		for (j = 97; j <= 122; j++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(j);
+			_putchar(ch);
+			ch++;
 		}
-		_putchar("\n");
+		_putchar('\n');
+		i++;
 	}
 }
