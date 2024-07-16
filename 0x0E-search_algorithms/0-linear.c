4f42b3509137a4434a362237f10c298c4045dc6d
MyssyB <beulahagiande@gmail.com>
2024-07-16 08:49:18 -0700

    betty

--- 0x0E-search_algorithms/0-linear.c
+++ 0x0E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
@@ -23,4 +23,3 @@ int linear_search(int *array, size_t size, int value)
 	}
 	return (-1);
 }
- 
