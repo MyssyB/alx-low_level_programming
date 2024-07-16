74b28cc981dcaba148da0de11f72768051509b73
MyssyB <beulahagiande@gmail.com>
2024-07-16 08:43:36 -0700

    searc algorithms

---
+++ 0x0E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Linear search alrorithm
+ * @array: pointer to the first element of the array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: number number otherwise -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
+ 
