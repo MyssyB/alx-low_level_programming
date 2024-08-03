2d5806c05d0519fed2e8282862007954cc854854
MyssyB <beulahagiande@gmail.com>
2024-08-03 11:04:06 -0700

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,24 +9,23 @@
 
 void rev_string(char *s)
 {
-	int count = 0, i = 0;
-	char last_char;
+	int length, j, i;
+	char v1, v2;
 
-	while (*s)
+	for (length = 0; s[length] != '\0'; length++)
 	{
-		s++;
-		count++;
 	}
-	for (i = 0; i < count; i++)
-	{
-		s--;
-		for (i = 0; i < (count / 2); i++)
-		{
-			int last = count - 1;
 
-			last_char = s[last - i];
-			s[last - i] = s[i];
-			s[i] = last_char;
-		}
+	j = length - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
+		i++;
 	}
 }
